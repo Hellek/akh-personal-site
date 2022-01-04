dcb15d6650d7d33b96c03c27a7e482e7e080ea1d
Roman Basharin <roman.basharin@quantori.com>
2022-01-04 20:06:53 +0300

    Add some color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      black: '#1e1e1e',
+      red: '#f0622c',
+    },
     fontFamily: {
       sans: ['"SuisseIntl"', 'sans-serif'],
     },
